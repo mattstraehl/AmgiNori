d6462c1bc545d55efaed1206407e96ace6dc481e
mattstraehl <masterstraehl@gmx.ch>
2016-05-01 20:53:31 +0200

    Update level configuration

--- app/src/main/java/com/matthias/android/amginori/Level.java
+++ app/src/main/java/com/matthias/android/amginori/Level.java
@@ -2,8 +2,8 @@ package com.matthias.android.amginori;
 
 public enum Level {
 
-    EASY(R.string.option_easy, 2, 5, 4500l),
-    HARD(R.string.option_hard, 3, 5, 3500l);
+    EASY(R.string.option_easy, 2, 3, 4000l),
+    HARD(R.string.option_hard, 10, 5, 3500l);
 
     public final int textId;
     public final int initialCardCount;
